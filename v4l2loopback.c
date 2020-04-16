9dc107926fa3affdfad5b4b4d2b2870db1c00e4e
Kara-Ali Aliev <k.aliev@samsung.com>
2020-04-16 11:03:17 +0300

    quick fix https://github.com/umlaeute/v4l2loopback/issues/172

--- v4l2loopback.c
+++ v4l2loopback.c
@@ -2092,7 +2092,7 @@ static void init_vdev(struct video_device *vdev, int nr)
 	vdev->ioctl_ops    = &v4l2_loopback_ioctl_ops;
 	vdev->release      = &video_device_release;
 	vdev->minor        = -1;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 0)
 	vdev->device_caps  =
 		V4L2_CAP_DEVICE_CAPS |
 #ifdef V4L2_CAP_VIDEO_M2M
